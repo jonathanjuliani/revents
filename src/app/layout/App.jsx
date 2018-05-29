54ecb680639055f17ffac0c8c1ec57d662fa158c
Jonathan Juliani <jonathanjuliani@outlook.com>
2018-05-29 16:29:24 -0300

    basic layout and functionality

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>REvents</h1>
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
