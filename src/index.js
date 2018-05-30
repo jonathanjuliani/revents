a0a8ebd9aba0754408164c596b700ae25caa2a80
Jonathan Juliani <jonathanjuliani@outlook.com>
2018-05-30 11:29:45 -0300

    setting up navigation with react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
@@ -8,13 +9,18 @@ import registerServiceWorker from './registerServiceWorker';
 const root = document.getElementById('root');
 
 let render = () => {
-    ReactDOM.render(<App />, root);
-}
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    root
+  );
+};
 
-if(module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render);
-    });
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
 }
 
 render();
